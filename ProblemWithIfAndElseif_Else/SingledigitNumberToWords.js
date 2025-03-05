a8c10a18d7c16536cf21a2e2883fa9f44d223694
Sonu Kumar <sonukumar31815@gmail.com>
2025-03-05 10:31:31 +0530

    [Sonu]Add. convert the single digit number to the words

---
+++ ProblemWithIfAndElseif_Else/SingledigitNumberToWords.js
@@ -0,0 +1,5 @@
+    // used to take command line argument
+    const n = parseInt(process.argv[2]); 
+
+    const words= ["Zero","One", "Two", "Three","Four","Five","Six","Seven","Eight","Nine"]; 
+    console.log(words[n]);
