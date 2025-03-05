be9b3d6b10d5b48e359955f2d5dce682f545c532
Sonu Kumar <sonukumar31815@gmail.com>
2025-03-05 11:08:50 +0530

    [Sonu]Add. Convert the number to words using switch case

---
+++ ProblemsWithCaseStatement/ConvertNumberToWords.js
@@ -0,0 +1,37 @@
+const n = parseInt(process.argv[2]);
+
+switch (n) {
+    case 0:
+        console.log("Zero");
+        break;
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    case 3:
+        console.log("Three");
+        break;
+    case 4:
+        console.log("Four");
+        break;
+    case 5:
+        console.log("Five");
+        break;
+    case 6:
+        console.log("Six");
+        break;
+    case 7:
+        console.log("Seven");
+        break;
+    case 8:
+        console.log("Eight");
+        break;
+    case 9:
+        console.log("Nine");
+        break;
+    default:
+        console.log("Invalid number, please enter a number between 0 and 9.");
+        break;
+}
